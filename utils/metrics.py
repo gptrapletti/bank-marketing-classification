cd08ccaa7480df2946b12c98f26ddda077222218
gptrapletti <giovanni.pievanitrapletti@gmail.com>
2023-12-01 00:00:38 +0100

    Add hyperparameter search

--- utils/metrics.py
+++ utils/metrics.py
@@ -31,3 +31,18 @@ def compute_metrics(preds, trues):
         'recall': round(recall, 2),
         'specificity': round(specificity, 2),       
     })
+    
+
+def specificity_score(preds, trues):
+    '''Computes specificity score for the prediction. Need this because sklearn's 'cross_val_score'
+    and 'cross_validate' don't have this metric.'''
+    
+    tp = ((trues == 1) * (preds == 1)).sum()
+    tn = ((trues == 0) * (preds == 0)).sum()
+    fp = ((trues == 0) * (preds == 1)).sum()
+    fn = ((trues == 1) * (preds == 0)).sum()
+    
+    specificity = tn / (tn + fp)
+    
+    return specificity
+    
